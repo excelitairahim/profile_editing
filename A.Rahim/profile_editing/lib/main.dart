e9e4aef520a630daa77ae719804ab3722e9f7904
excelitrahim <rahim@excelitai.com>
2022-08-17 11:27:13 +0600

    change class name

--- A.Rahim/profile_editing/lib/main.dart
+++ A.Rahim/profile_editing/lib/main.dart
@@ -1,45 +1,4 @@
-// import 'dart:convert';
 
-// import 'package:fashion_design/otp_verification/otp_senderPage.dart';
-// import 'package:fashion_design/providers/cart_provider.dart';
-// import 'package:fashion_design/providers/login_signup.dart';
-// import 'package:fashion_design/providers/pagination_provider.dart';
-// import 'package:fashion_design/providers/product_provider.dart';
-// import 'package:fashion_design/providers/tokenstoreprovider.dart';
-// import 'package:fashion_design/screens/bottomnav_bar.dart';
-// import 'package:fashion_design/screens/login_signup.dart/login_and_signup.dart';
-// import 'package:fashion_design/screens/pagination_page.dart';
-// import 'package:fashion_design/screens/product_list.dart';
-// import 'package:fashion_design/screens/product_page.dart';
-// import 'package:fashion_design/screens/products_details.dart';
-// import 'package:firebase_core/firebase_core.dart';
-// import 'package:flutter/material.dart';
-// import 'package:flutter_html/shims/dart_ui_real.dart';
-// import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
-// import 'package:provider/provider.dart';
-
-// import 'Google_login/signin_screen.dart';
-
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await Firebase.initializeApp();
-//   runApp(MultiProvider(
-//     providers: [
-//       ChangeNotifierProvider<CartProvider>(
-//           create: ((context) => CartProvider())),
-//       ChangeNotifierProvider<ProductProvider>(
-//           create: ((context) => ProductProvider())),
-//       ChangeNotifierProvider<SignupLogicModel>(
-//           create: ((context) => SignupLogicModel())),
-//        ChangeNotifierProvider<TokenProvider>(
-//       create: ((context) => TokenProvider())),
-//     ],
-//     child: MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       home: LogginScreen(),
-//     ),
-//   ));
-// }
 
 import 'dart:io';
 
@@ -61,9 +20,10 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: SettingsUI()
+        home: EditProfilePage(),
+        //SettingsUI()
         // HomePage()
-        //MyHomePage1(),
+      //  MyHomePage1(),
         );
   }
 }
@@ -200,16 +160,7 @@ Widget buildSocialButton(
       onTap: onClicked,
     );
 
-class SettingsUI extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Setting UI",
-      home: EditProfilePage(),
-    );
-  }
-}
+
 
 class EditProfilePage extends StatefulWidget {
   @override
